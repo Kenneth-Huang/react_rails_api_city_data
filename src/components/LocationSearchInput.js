ac2076cfb01eb5a12ce690da961d31d1e3e98120
anne <richardson.ae@gmail.com>
2018-05-17 16:42:40 -0500

    Removes unused parts of template code

--- src/components/LocationSearchInput.js
+++ src/components/LocationSearchInput.js
@@ -1,34 +1,28 @@
 import React from 'react'
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import PlacesAutocomplete, { geocodeByAddress } from 'react-places-autocomplete'
 
 class LocationSearchInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = { address: '' }
-    // this.setFormLocation = this.props
   }
-
-  // googlePlaceRef = React.createRef()
 
   handleChange = (address) => {
     this.setState({ address })
   }
 
-  // .then(results => getLatLng(results[0]))
   handleSelect = (address) => {
     const setFormLocation = this.props.setFormLocation
 
     geocodeByAddress(address)
       .then(function(results){
         setFormLocation(results[0].formatted_address)
-        getLatLng(results[0])
       })
-      .then(latLng => console.log('Success', latLng))
       .catch(error => console.error('Error', error))
   }
 
   render() {
-    const renderFunc = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
+    const renderInputField = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
       <div className="autocomplete-root">
         <input className="form-control" {...getInputProps()} />
         <div className="autocomplete-dropdown-container">
@@ -53,7 +47,7 @@ class LocationSearchInput extends React.Component {
         onSelect={this.handleSelect}
         searchOptions={searchOptions}
       >
-        {renderFunc}
+        {renderInputField}
       </PlacesAutocomplete>
     );
   }
